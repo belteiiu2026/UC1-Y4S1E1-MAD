4e8cf6b0b04897fd79c0accd6e8ec5cce695c4a3
Chhai Chivon <chivon.chhai.kh@gmail.com>
2026-04-10 20:37:17 +0700

    MAD for E1

---
+++ lib/startup_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StartupScreen extends StatelessWidget {
+
+  const StartupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Hi, MAD"),
+      ),
+    );
+  }
+}
